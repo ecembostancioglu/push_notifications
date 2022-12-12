b9c0120f8cd4699b06dca1aabb2be3b2c2af756a
Ecem Bostancıoğlu <ecembostancioglu21@gmail.com>
2022-12-12 18:04:51 +0300

    Added one signal package and initialized

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+
+  @override
+  void initState() {
+    dotenv.testLoad(fileInput: 'APPID');
+    super.initState();
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Home(),
+    );
   }
 }
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+//DOT ENV SIKINTILI
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 import 'app.dart';
 
 void main()async {
-  await dotenv.load(fileName: '.env');
+  await dotenv.load(fileName: ".env");
   runApp(const App());
+
+  String key=dotenv.get("APPID");
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+  OneSignal.shared.setAppId(key);
+  OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+    print('Accepted permission: $accepted');
+  } );
+  
+  
 }
 
